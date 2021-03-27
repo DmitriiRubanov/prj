61e8f6d999aa9e7dc49e1aa243a8d9bff879e6b4
secret <dmitrii.rubanov@yandex.ru>
2021-03-27 11:01:16 +0300

    1

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def classic():
+    return render_template('base.html', title='Заголовок')
+
+
+@app.route('/index/<title>')
+def index(title):
+    if title:
+        return render_template('base.html', title=title)
+    else:
+        return redirect('/index')
+
+
 @app.route('/index')
-def index():
-    return render_template('base.html', title=f'Заголовок')
+def ix():
+    return render_template('base.html', title='Заголовок')
 
 
 if __name__ == '__main__':
